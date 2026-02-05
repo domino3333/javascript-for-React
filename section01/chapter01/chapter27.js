168a360a5aff655831acc3ef03a3ae33f35bbe12
domino3333 <busster@naver.com>
2026-02-05 10:39:13 +0900

    객체 깊은 복사 후 json문자열로 변환

---
+++ section01/chapter01/chapter27.js
@@ -0,0 +1,13 @@
+let object1 = { name: "홍길동", age: 30 };
+
+let object2 = { 
+  ...object1,
+};
+
+object2["age"] = 10;
+
+console.log(object2);
+
+console.log(JSON.stringify(object1) === JSON.stringify(object2));
+
+
