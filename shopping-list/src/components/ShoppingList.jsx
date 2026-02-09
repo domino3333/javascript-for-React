48e2bdfa78cfd601c36dc1d86ff6387b6cf790cb
domino3333 <busster@naver.com>
2026-02-09 12:16:55 +0900

    이름을 기준으로 검색 기능 구현 완료

--- shopping-list/src/components/ShoppingList.jsx
+++ shopping-list/src/components/ShoppingList.jsx
@@ -1,13 +1,42 @@
 import ShoppingItem from "./ShoppingItem";
+import { useState } from "react";
 
 const ShoppingList = ({ goods }) => {
 
 
-  return (<>
-    {goods.map((item) =>
-      (< ShoppingItem key={item.id}{...item} />)
+  //검색창에 입력된 텍스트를 state
+  const[inputData, setInputData] = useState("");
+  //실제로 검색할 키워드 state
+  const[completedKeyword,setCompletedKeyword] = useState("");
 
+  const observeNameData = (e)=>{
+    setInputData(e.target.value);
+  }
+
+  const clickSearchButton = ()=>{
+    //클릭버튼 누르면 그 키워드를 기준으로 필터된 것으로 다시 화면을 그려야 함
+    setCompletedKeyword(inputData);
+  }
+
+  function filteredData(){
+
+    if(completedKeyword ==='') return goods;
+    const data = goods.filter((item)=>
+      item.name.includes(completedKeyword)
+    )
+
+    return data;
+  }
+
+  const filteredDatas = filteredData();
+
+  return (<>
+    {filteredDatas.map((item) =>
+      (< ShoppingItem key={item.id}{...item} />)
     )}
+    <br/>
+    <input onChange={observeNameData}type="text" placeholder="검색할 상품의 이름"/>
+    <button type="button" onClick={clickSearchButton}>검색</button>
   </>)
 
 
