2561180492ff24fe636956b92f73f74b327231aa
domino3333 <busster@naver.com>
2026-02-12 14:55:25 +0900

    diaryList.jsx / css 수정

--- section12/src/components/DiaryList.jsx
+++ section12/src/components/DiaryList.jsx
@@ -1,23 +1,21 @@
 import Button from "./Button";
-
+import DiaryItem from "./DiaryItem";
+import './DiaryList.css'
 
 const DiaryList = () => {
-
-  return (<>
+  return (
     <div className="DiaryList">
-      <div className="menuBar">
-        <select name="" id="">
-          <option value={"lastest"}>LATEST</option>
-          <option value={"oldest"}>OLDEST</option>
+      <div className="menu_bar">
+        <select>
+          <option value={"latest"}>최신순</option>
+          <option value={"oldest"}>오래된 순</option>
         </select>
-        <Button text={"새 일기 쓰기"} type={"POSITIVE"}/>
+        <Button text={"새 일기 쓰기"} type={"POSITIVE"} />
       </div>
       <div className="list_wrapper">
-        <DiaryItem/>
+        <DiaryItem />
       </div>
-
     </div>
-  </>)
-}
-
+  );
+};
 export default DiaryList;
