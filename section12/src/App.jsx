6fb8385af49477a64d247c47c2a93edfdaf2b6f0
domino3333 <busster@naver.com>
2026-02-12 11:43:17 +0900

    이 상태에서 npm run build(배포) 하면 dist 라는 폴더가 생기고 그 안에는 public이 들어감 assets에 있는 이미지는 네트워크를 통해 주고 받는다, 빌드완료 후 npm run preview로 확인 가능, 4173포트 웹 들어가서 F12 -> elements 를 보면 assets에서 온 이미지는 src가 신기하게 적혀있음 F12에서 networks탭을 보면 public에 들어가는건 요청할 때마다 시간이 걸리고, assets은 바로 가져올 수 있다. 자주 요청하지 않는 것은 public / 자주 요청하는 것은 assets에 저장하기로 한다

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -6,6 +6,10 @@ import Diary from './components/Diary'
 import Notfound from './components/Notfound'
 import Edit from './components/Edit'
 import emotion1 from './assets/emotion1.png'
+import emotion2 from './assets/emotion2.png'
+import emotion3 from './assets/emotion3.png'
+import emotion4 from './assets/emotion4.png'
+import emotion5 from './assets/emotion5.png'
 
 function App() {
 
@@ -16,10 +20,17 @@ function App() {
       {/* 공통 부분 */}
       <h2>public img</h2>
       <img src="/emotion11.png"/>
+      <img src="/emotion12.png"/>
+      <img src="/emotion13.png"/>
+      <img src="/emotion14.png"/>
+      <img src="/emotion15.png"/>
 
       <h2>assets img</h2>
       <img src={emotion1}/>
-      <img src="./assets/emotion1.png"/>
+      <img src={emotion2}/>
+      <img src={emotion3}/>
+      <img src={emotion4}/>
+      <img src={emotion5}/>
 
       <Routes>
         <Route path='/' element={<Home />} />
