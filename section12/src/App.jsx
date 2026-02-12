ef30fb09a151470fa51b0a8bbfc6043b53af4910
domino3333 <busster@naver.com>
2026-02-12 15:11:30 +0900

    App.jsx에 mockData, reducer 추가

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -5,7 +5,44 @@ import New from './components/New'
 import Diary from './components/Diary'
 import Notfound from './components/Notfound'
 import Edit from './components/Edit'
-import Header from './components/Header'
+
+//가상데이터
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+];
+
+//useReducer
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [...state,action.data];
+    case "UPDATE":
+      return state.map((item) =>
+        String(item.id) === String(action.data.id)
+          ? action.data
+          : item
+      );
+    case "DELETE":
+      return state.filter(
+        (item) => String(item.id) !== String(action.id)
+      );
+    default:
+      return state;
+  }
+}
+
+
 
 function App() {
 
